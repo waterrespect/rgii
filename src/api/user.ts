c69004ed2fa7b59518be0b1df2b7a6fb75ae5b4a
waterrespect <747483083@qq.com>
2023-10-11 23:09:50 +0800

    main

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,26 +1,28 @@
 import request from '@/utils/request'
-import { loginForm } from '../api/model/user_class'
+import type { loginForm } from '../model/user_class'
 
 const api_name = `/api/login`
 
-export async function login(data: loginForm) {
-  return request({
-    url: `${api_name}/login`,
-    method: 'post',
-    data,
-  })
-}
+export default {
+  login(data: loginForm) {
+    return request({
+      url: `${api_name}/login`,
+      method: 'post',
+      data,
+    })
+  },
 
-export function logout() {
-  return request({
-    url: '/logout',
-    method: 'post',
-  })
-}
+  logout() {
+    return request({
+      url: '/logout',
+      method: 'post',
+    })
+  },
 
-export function register() {
-  return request({
-    url: '/register',
-    method: 'post',
-  })
+  register() {
+    return request({
+      url: '/register',
+      method: 'post',
+    })
+  }
 }
